d2befc6d3d9fa75d4ade376fe729bd6f5c2267e3
Taeun Lee <tulee3474@naver.com>
2023-05-01 21:58:37 +0900

    [FEAT] git add 등 각각의 명령어를 입력했을때, 레포지토리 객체 내의 변화를 주는 함수들 제작

--- gitRepository.py
+++ gitRepository.py
@@ -9,7 +9,7 @@ class gitRepository:
         self.dirName = name
 
 
-def gitRepositoryCreation(os, currentPath, repo):    #현재Path를 인풋값으로 작동
+def gitRepositoryCreation(os, currentPath, repo):    #현재 Path를 인풋값으로 작동
     #이 서비스는 모든 로컬 디렉터리를 깃 저장소로 전환할 수 있도록 지원합니다.
 
     # Get all Files and Folders from the given Directory
@@ -22,6 +22,38 @@ def gitRepositoryCreation(os, currentPath, repo):    #현재Path를 인풋값으
             print(item)
             gitRepositoryCreation(os, currentPath + "\\" + item, repo)    #재귀
 
-
     print(repo)
 
+
+def gitAdd(file, repo):    # git add
+    repo.staged.append(file)    #staged에 넣음
+    if file in repo.modified:    #(modified -> staged;인 경우, modified에서도 삭제해야함
+        repo.modified.remove(file)
+
+def gitRestore(file, repo):    # git restore
+    if file in repo.modified:    #modified -> unmodified;인 경우
+        repo.modified.remove(file)
+        repo.unmodified.append(file)
+        # TODO file의 상태를 수정 전으로 복구하는 작업 추가 필요
+    elif file in repo.staged:    #staged -> modified or untracked;인 경우 -> git restore --staged
+        repo.staged.remove(file)
+        repo.modified.append(file)    #일단 modified로 넣음. 이걸 구분하는 건 없길래
+
+def gitRM(file, repo):    # git rm
+    #committed -> staged;
+    repo.committed.remove(file)
+    repo.staged.append(file)
+
+def gitRMCached(file, repo):    # git rm --cached
+    #committed -> untracked;
+    repo.committed.remove(file)
+
+
+def gitMV(file, repo):    # git mv
+    #committed -> staged;
+    repo.committed.remove(file)
+    repo.staged.append(file)
+
+def gitCommit(file, repo):    # git commit
+    repo.staged.remove(file)
+    repo.committed.append(file)
